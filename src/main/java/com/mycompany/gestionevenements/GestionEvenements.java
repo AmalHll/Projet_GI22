8a5551c8660f0215daf1ef31f867ffbc8df0beff
AmalHl <amalhallou@gmail.com>
2026-04-10 11:08:20 +0100

    ajout classe participant

---
+++ src/main/java/com/mycompany/gestionevenements/GestionEvenements.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.gestionevenements;
+
+/**
+ *
+ * @author Amal
+ */
+public class GestionEvenements {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
